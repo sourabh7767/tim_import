e7d67c11b2d307bd64556aef5f9e4e7f654a6bc7
Sourabh <saurav7767@gmail.com>
2024-08-19 21:36:05 +0530

    updates

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('prevent-back-history')->group(function (){
 
     Route::get('/clear-cache', function () {
-        Artisan::call('cache:clear');
+        Artisan::call('cache:clear'); 
         Artisan::call('route:clear');
         Artisan::call('config:clear'); 
         Artisan::call('view:clear');
